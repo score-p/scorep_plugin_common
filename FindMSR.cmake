d16e325923885238e2ebc31d8991a2254ba0aa91
Michael Werner <xaseron@googlemail.com>
2016-02-09 17:05:48 +0100

    fixed path

--- FindMSR.cmake
+++ FindMSR.cmake
@@ -2,10 +2,10 @@ find_path(MSR_LIB_DIR ${LIBMSR_NAME} HINTS ${MSR_LIB} ${MSR_DIR}/lib ENV LD_LIBR
 
 if( MSR_LIB_DIR )
 
-	find_path(MSR_INC_DIR msr.h HINTS ${MSR_INC} ${MSR_LIB_DIR}../include ${MSR_DIR}/include DOC "Path to msr.h")
+	find_path(MSR_INC_DIR msr.h HINTS ${MSR_INC} ${MSR_LIB_DIR}/../include ${MSR_DIR}/include DOC "Path to msr.h")
 
 	if( MSR_INC_DIR )
-    	set(MSR_FOUND true)
+        set(MSR_FOUND true)
 	else()
 	    message(STATUS "Could NOT find libmsr header (missing: MSR_DIR / MSR_INC )")
 	endif()
