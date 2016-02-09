460cce23b0de1df4a117d2a163e691021460f662
Michael Werner <xaseron@googlemail.com>
2016-02-09 17:10:29 +0100

    added cmake to find papi

---
+++ FindPAPI.cmake
@@ -0,0 +1,16 @@
+find_path(PAPI_LIB_DIR libpapi.so HINTS ${PAPI_LIB} ${PAPI_DIR}/lib ENV LD_LIBRARY_PATH DOC "Path to PAPI")
+
+if( PAPI_LIB_DIR )
+
+
+    find_path(PAPI_INC_DIR papi.h HINTS ${PAPI_INC} ${PAPI_LIB_DIR}/../include ${PAPI_DIR}/include DOC "Path to papi.h")
+
+    if( PAPI_INC_DIR )
+        set(PAPI_FOUND true)
+    else()
+        message(STATUS "Could NOT find libx86_adapt header (missing: X86A_DIR / X86A_INC )")
+    endif()
+else()
+    set(PAPI_FOUND false)
+    message(STATUS "Could NOT find PAPI (missing: PAPI_DIR / PAPI_LIB )")
+endif()
