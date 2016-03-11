b7b802c4b31cf2d45ffb2abd7e2838fe171d73c3
Philipp Trommler <philipp.trommler@mailbox.tu-dresden.de>
2016-03-11 12:44:08 +0100

    Added/Corrected licence files and file headers.

--- FindLibDataheap.cmake
+++ FindLibDataheap.cmake
@@ -1,3 +1,28 @@
+# Copyright (c) 2016, Technische Universität Dresden, Germany
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted
+# provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+#    and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+#    and the following disclaimer in the documentation and/or other materials provided with the
+#    distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse
+#    or promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 # Find libdataheap
 if (LIBDATAHEAP_LIBRARIES AND LIBDATAHEAP_INCLUDE_DIRS)
   set (LibDataheap_FIND_QUIETLY TRUE)
@@ -11,4 +36,4 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibDataheap DEFAULT_MSG
   LIBDATAHEAP_LIBRARIES
   LIBDATAHEAP_INCLUDE_DIRS)
 
-mark_as_advanced(LIBDATAHEAP_INCLUDE_DIRS LIBDATAHEAP_LIBRARIES) 
+mark_as_advanced(LIBDATAHEAP_INCLUDE_DIRS LIBDATAHEAP_LIBRARIES)
